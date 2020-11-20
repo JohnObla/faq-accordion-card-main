0f4a344326799e220c3cebbb005dd77a024c288c
John Obla <john-obla@live.co.uk>
2020-11-20 19:21:00 +0000

    Toggled hide answer

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const questions = document.body.querySelectorAll('.question');
 for (const question of questions) {
     question.addEventListener('click', () => {
         question.classList.toggle('bold');
+        question.nextElementSibling.classList.toggle('hide');
     })
 }
