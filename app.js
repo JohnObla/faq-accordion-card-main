0fed8d30c8a1c8462ea12483aba7eba9852ffccc
John Obla <john-obla@live.co.uk>
2020-11-21 01:51:21 +0000

    Arrow now rotates on click

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const answers = document.body.querySelectorAll('.answer');
 function expandQuestion(qa) {
     const question = qa.children[0];
     const answer = qa.children[1];
+    const arrow = question.children[0];
 
     qa.classList.add('expanded')
     question.classList.add('bold');
     answer.classList.remove('hide');
+    arrow.classList.add('flip');
 }
 
 function closeQuestion(qa) {
     const question = qa.children[0];
     const answer = qa.children[1];
+    const arrow = question.children[0];
 
     qa.classList.remove('expanded');
     question.classList.remove('bold');
+    arrow.classList.remove('flip');
     answer.classList.add('hide');
 }
 
